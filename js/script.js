dec78ec8bc1c581b8076015876cc070a14d897c4
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2023-11-18 12:24:11 +0100

    Cargando documento corregido en clase

--- js/script.js
+++ js/script.js
@@ -1,58 +1,51 @@
+const characterList = document.getElementById('character-list')
+const prevPage = document.getElementById('prev-page')
+const nextPage = document.getElementById('next-page')
+let next
+let prev
+let page = 'https://rickandmortyapi.com/api/character/?page=1'
 
+let paginaActual = 1
 
-//Los elementos que tienes que imprimir dentro de li son name , image voy a intentar crear una estructura como 
-//en el ejemplo
-
-const characterList = document.getElementById('character-list');
-
-//Creado un elemento lista
-const li= document.createElement("li")
-
-//le he dicho que  el elemento que he creado es hijo de ul
-characterList.appendChild(li);
-
-
-//Los elementos que tienes que imprimir dentro de li son: name e image voy a intentar crear una estructura como 
-//en el ejemplo
- // for (const element of characterListId){
- // let templateRickeMorty = `
- //<img src= "https://rickandmortyapi.com/api/${element.episode}" alt="${element.name}"/>
-  //<h3>${element.name}</h3>
-  //</div>`
-
-//li.innerHTML += templateRickeMorty;
-
-
-
-//obtenerChiste.addEventListener('click', () => {
-  let rickeMorthy =fetch('https://rickandmortyapi.com/api/character/?page=1.')
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('La solicitud no fue exitosa');
-      }
-      return response.json();
-    })
-   .then((data) => {
-    for (i=0;i<rickeMorthy.length;i++){
-      console.log(data.results[i].name)
-    }
+function getFetchRickandMortyCharacter(page) {
+  characterList.innerHTML = ''
+  fetch(page)
+  .then((response) => {
+    if(!response.ok) {
+      throw new Error('La solicitud no fue exitosa')
+    } return response.json()
+  })
+  .then((data) => {
     
+    const characters = data.results
+    const info = data.info
+    next = data.info.next
+    prev = data.info.prev
 
-    //intento crear una estructura template anidada en el fetch para 
-       //  for (const element of characterListId){
-       //let templateRickeMorty = `
-      //<img src= "https://rickandmortyapi.com/api/${element.next}" alt="${element.name}"/>
-      //<h3>${element.name}</h3>
-      //</div>`
 
-      //li.innerHTML += templateRickeMorty;
-      //   }
+    console.log(info)
+    characters.forEach(character => {
+      let listItem = `
+        <li>
+          <img src="${character.image}" alt='${character.name}' />
+          <p><span>Nombre: </span>${character.name}</p>
+          <p><span>Especie: </span>${character.species}</p>
+        </li>
+      `
+      characterList.innerHTML += listItem
+    })
+  })
+}
 
-   })
-    .catch((error) => {
-      li.innerText = 'algo estas haciendo mal, pero mejor que ayer felicitate';
-    });
-//});
+nextPage.addEventListener('click', function() {
+  getFetchRickandMortyCharacter(next)
+})
+
+prevPage.addEventListener('click', function() {
+  getFetchRickandMortyCharacter(prev)
+})
+
+getFetchRickandMortyCharacter(page)
 
 
 
